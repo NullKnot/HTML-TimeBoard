12526866a1ccc8306fa8ece2e0dec56ab2198fd8
NullKnow <terroson8422@gmail.com>
2016-04-24 22:15:11 +0800

    commit2

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function labelStyle(locX, locY, fSize, fColor, rAngle){
 	this.fColor = fColor;
 	this.rAngle = rAngle;
 }
-var labelStyle0 = new labelStyle("280px","350px","55px","gray",10);
-var labelStyle1 = new labelStyle("220px","300px","80px","red",13);
-var labelStyle2 = new labelStyle("320px","480px","100px","brown",358);
-var labelStyle3 = new labelStyle("230px","450px","100px","green",2);
-var labelStyle4 = new labelStyle("290px","420px","65px","deeppink",0);
+var labelStyle0 = new labelStyle("280px","390px","55px","gray",10);
+var labelStyle1 = new labelStyle("220px","290px","80px","red",13);
+var labelStyle2 = new labelStyle("300px","360px","100px","brown",358);
+var labelStyle3 = new labelStyle("230px","250px","100px","green",2);
+var labelStyle4 = new labelStyle("290px","450px","65px","deeppink",0);
 
 var labelStyleArray = [labelStyle0, labelStyle1, labelStyle2, labelStyle3, labelStyle4];
 
@@ -27,7 +27,7 @@ window.onload = function(){
 	changeBgImage();
 	updateTimeLabel(); 
 	
-	setInterval(changeBgImage, 5000);
+	setInterval(changeBgImage, 1000);
 	setInterval(updateTimeLabel, 1000);
 }
 
